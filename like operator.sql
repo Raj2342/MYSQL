4b3427c85ec85f2bad68cea1de9f508e2531ff0a
Raj <120565750+Raj2342@users.noreply.github.com>
2024-05-29 18:45:18 +0530

    6:45

---
+++ like operator.sql
@@ -0,0 +1,8 @@
+use wscube;
+-- Select*from student where name like "p%" and gender="M"; 
+-- Select*from student where name like "%a" ; 
+-- Select*from student where name like "%i%"; 
+-- Select name,city,gender from student where  city like "_a%"
+ -- Select*from student where name like "p____"; -- parul , ____ = 4 dash 
+-- Select*from student where name like "p____%";  -- piyush , prince , parul , ____ = 4 dash+ multiple character
+ Select*from student where name like "r%a"; 
---
+++ student table 9 data.sql
@@ -0,0 +1,10 @@
+use wscube;
+insert into student( id , name , email,city,gender) 
+				  values( 2,"Riya" , "riyakumari@gmail.com", "kanpur","F"),
+                  ( 3,"john" , "john@gmail.com", "America","M"),
+                     ( 4,"Ankur" , "Ankur@gmail.com", "mamupur","M"),
+                        ( 5,"dimple" , "dimple@gmail.com", "mohali","F"),
+                           ( 6,"smita" , "smita@gmail.com", "morinda","F"),
+                              ( 7,"piyush" , "piyush@gmail.com", "rampur","M"),
+                                 ( 8,"prince" , "prince@gmail.com", "sitapur","M"),
+                                    ( 9,"parul" , "parul@gmail.com", "palampur","F");
