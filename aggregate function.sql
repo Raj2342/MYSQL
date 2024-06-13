651cf35e156bc0325a811650bf5fc4a5088ab903
Raj <120565750+Raj2342@users.noreply.github.com>
2024-06-13 22:43:16 +0530

    Add files via upload

---
+++ aggregate function.sql
@@ -0,0 +1,15 @@
+use  wscube;
+-- count 
+select  count(id) dui from best_student where  age>21;
+
+-- sum 
+select sum(age) from best_student;
+
+-- average
+select avg(age) from best_student;
+
+-- min
+select min(age) from best_student;
+
+-- max 
+select max(age) from best_student;
