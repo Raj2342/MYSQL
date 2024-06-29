ac3c523e659c1d172950cc44865866da3489ff92
Raj <120565750+Raj2342@users.noreply.github.com>
2024-06-29 23:53:03 +0530

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,15 +1,22 @@
-
--- create view student_data
--- as
--- select id, name , city  from best_student
--- inner join cities
--- on best_student.cid_id= cities.cid;
-
+/*
+ create view student_data
+as
+select id, name , city  from best_student
+inner join cities
+on best_student.cid_id= cities.cid;
+*/
 -- alter
+/*
 alter view student_data
 as
 select id, name , city  from best_student
 inner join cities
 on best_student.cid_id= cities.cid;
+*/
 
--- drop cammand , alter , create , trucate 
+ -- select id , name ,city from student_data  -- use database other wise show error
+ -- rename table 
+ 
+ RENAME table student_data
+ to core_student;
+ Select*from core_student;
